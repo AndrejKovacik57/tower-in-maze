ac8dce3c1f041690c0378eaddd9b585dff22919a
AndrejKovacik57 <xkovacika1@stuba.sk>
2021-04-18 23:15:56 +0200

    changing maze to arraylist of arraylists

--- src/main/java/sk/stuba/fei/uim/oop/Maze.java
+++ src/main/java/sk/stuba/fei/uim/oop/Maze.java
@@ -11,8 +11,7 @@ public class Maze extends JPanel {
     private int rows;
     private int columns;
     private int cellSize;
-    private ArrayList<Cell> maze = new ArrayList<Cell>();
-
+    private ArrayList<Cell>[][] maze = new ArrayList[10][10];
 
     public Maze(int size, int rowColNum){
         System.out.println("konstruktor maze");
@@ -20,6 +19,7 @@ public class Maze extends JPanel {
         this.rows=rowColNum;
         this.columns=rowColNum;
         this.cellSize=size/rowColNum;
+
         createMaze();
         createPanel();
 
@@ -27,15 +27,33 @@ public class Maze extends JPanel {
 
     public void createMaze(){
         System.out.println("create maze size "+size+" col "+columns+", row "+rows);
+        maze = new ArrayList[columns][rows];
         for (int x=0; x<columns;x++){
             System.out.println("add cell for 1");
             for (int y=0; y<rows;y++){
-                maze.add(new Cell(false,true,true,true,true,x,y,size/rows));
+                //maze.add(new Cell(false,true,true,true,true,x,y,size/rows));
+                maze[x][y]=new ArrayList<Cell>();
+                maze[x][y].add(new Cell(false,true,true,true,true,x,y,size/rows));
                 System.out.println("add cell for 2");
 
             }
         }
     }
+    /*public Cell findCell(int x,int y){
+
+        for(Cell cell: maze){
+            if(cell.getXIndex()==x && cell.getYIndex()==y){
+                return cell;
+            }
+        }
+        return null;
+    }*/
+    public void recursiveRandomDFS(int x,int y){
+        //findCell(x,y).setVisited(true);
+
+
+    }
+
     public void createPanel(){
         this.setPreferredSize(new Dimension(size+(columns+1)*gridSize,size+(rows+1)*gridSize));
         this.setBackground(Color.GREEN);
@@ -50,19 +68,22 @@ public class Maze extends JPanel {
         g2D.setStroke(new BasicStroke(gridSize));
         System.out.println("maze paint");
 
-        for (int i=0; i< maze.size(); i++){
-            if(maze.get(i).isTopWall()){
-                g2D.drawLine(maze.get(i).getXIndex()*cellSize, maze.get(i).getYIndex()*cellSize+1, maze.get(i).getXIndex()*cellSize+cellSize, maze.get(i).getYIndex()*cellSize+1);
-            }
-            if(maze.get(i).isBottomWall()){
-                g2D.drawLine(maze.get(i).getXIndex()*cellSize, maze.get(i).getYIndex()*cellSize+cellSize+1, maze.get(i).getXIndex()*cellSize+cellSize, maze.get(i).getYIndex()*cellSize+cellSize+1);
-            }
-            if(maze.get(i).isLeftWall()){
-                g2D.drawLine(maze.get(i).getXIndex()*cellSize, maze.get(i).getYIndex()*cellSize, maze.get(i).getXIndex()*cellSize, maze.get(i).getYIndex()*cellSize+cellSize);
-            }
-            if(maze.get(i).isRightWall()){
-                g2D.drawLine(maze.get(i).getXIndex()*cellSize+cellSize, maze.get(i).getYIndex()*cellSize, maze.get(i).getXIndex()*cellSize+cellSize, maze.get(i).getYIndex()*cellSize+cellSize);
+        for (int x=0; x<columns;x++){
+            System.out.println("add cell for 1");
+            for (int y=0; y<rows;y++){
+                if(maze[x][y].get(0).isTopWall()){
+                    g2D.drawLine(maze[x][y].get(0).getXIndex()*cellSize, maze[x][y].get(0).getYIndex()*cellSize+1, maze[x][y].get(0).getXIndex()*cellSize+cellSize, maze[x][y].get(0).getYIndex()*cellSize+1);
+                }
+                if(maze[x][y].get(0).isBottomWall()){
+                    g2D.drawLine(maze[x][y].get(0).getXIndex()*cellSize, maze[x][y].get(0).getYIndex()*cellSize+cellSize+1, maze[x][y].get(0).getXIndex()*cellSize+cellSize, maze[x][y].get(0).getYIndex()*cellSize+cellSize+1);
+                }
+                if(maze[x][y].get(0).isLeftWall()){
+                    g2D.drawLine(maze[x][y].get(0).getXIndex()*cellSize, maze[x][y].get(0).getYIndex()*cellSize, maze[x][y].get(0).getXIndex()*cellSize, maze[x][y].get(0).getYIndex()*cellSize+cellSize);
+                }
+                if(maze[x][y].get(0).isRightWall()){
+                    g2D.drawLine(maze[x][y].get(0).getXIndex()*cellSize+cellSize, maze[x][y].get(0).getYIndex()*cellSize, maze[x][y].get(0).getXIndex()*cellSize+cellSize, maze[x][y].get(0).getYIndex()*cellSize+cellSize);
 
+                }
             }
         }
 
